227775371931d18b254f04d0845216a78878d5c2
whatvlad <78967737+whatvlad@users.noreply.github.com>
2021-12-30 14:56:23 +0200

    Add files via upload

---
+++ classwork/ls25.py
@@ -0,0 +1,29 @@
+# class Human:
+#    pass
+# p1=Human()
+# p1.name="Vlad"
+# p1.surname="Tkachenko"
+# p1.place_of_birth="UA"
+# p1.year_of_birth=2005
+# p1.age=16
+# print(p1.name)
+# print(f"Name: {p1.name}, Surname: {p1.surname}, Birth: {p1.place_of_birth}, Year: {p1.year_of_birth}, Age: {p1.age}")
+
+
+# class Human:
+#   def __init__(self,name,surname,place_of_birth,year_of_birth,age):
+#     self.name=name
+#     self.surname=surname
+#     self.place_of_birth=place_of_birth
+#     self.year_of_birth=year_of_birth
+#     self.age=age
+#     print("Человек создан")
+#   def hello(self):
+#     print(f"Name: {self.name}, Surname: {self.surname}, Place of birth: {self.place_of_birth}, Year of birth: {self.year_of_birth}, Age: {self.age}")
+#   def ages(self):
+#     a=2021
+#     return a-self.year_of_birth
+# p1=Human("Den", "Dmitriev", "UA", 1997, 24)
+# p1.hello()
+# p2=Human("Vlad", "Tkachenko", "UA", 2005, 16)
+# p2.hello()
