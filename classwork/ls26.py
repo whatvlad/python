b34195e8c6be423a5dd50d9f4a52946a6616342b
whatvlad <vladennopwnz@gmail.com>
2022-02-19 14:58:42 +0200

    Add files via upload

---
+++ classwork/ls26.py
@@ -0,0 +1,50 @@
+    #1
+# class Human:
+#     def __init__(self,name,surname,age,birth):
+#         self.name=name
+#         self.surname=surname
+#         self.age=age
+#         self.birth=birth
+
+#     def info(self):
+#         print(f"NAME:{self.name},surname:{self.surname},age:{self.age},birth:{self.birth} ")
+# class Student(Human):
+#     def study(self):
+#         print(f"Student:{self.name} studding")
+# class Techer(Human):
+#     def teach(self):
+#         print(f"Student:{self.name} teaching")
+# new_s=Student("Bogdan","Kinakh","KR",2003)
+# new_s.info()
+    #2
+# class Animals:
+#     def __init__(self,name,age,color,studu,time):
+#         self.name=name
+#         self.age=age
+#         self.color=color
+#         self.studu=studu
+#         self.time=time
+#     def info(self):
+#         print(f"Name:{self.name},Age:{self.age},Color:{self.color},Studu:{self.studu},Time:{self.time}")
+# class Dog(Animals):
+#     def dog(self):
+#         print(f"Dog:{self.name}DogI")
+#     def breakfest(self):
+#         print(f"Он ест в 2 часа дня)
+# class Cat(Animals):
+#     def cat(self):
+#         print(f"Cat:{self.name}CatI")  
+#     def seating(self):
+#         print(f"Он сидит по много часов в день)
+# class Frog(Animals):
+#     def frog(self):
+#         print(f"Frog:{self.name}Frog")
+# new_s=Dog("Dog",1,"Black","Eating","2 minuts")
+# new_s.info()
+# print ("----"*20)
+# new_s=Cat("Cat",12,"Grey","seating","2 hours")
+# new_s.info()
+# print ("----"*20)
+# new_s=Frog("Frog",24,"Green","KVA-kVa-KvA","5 second")
+# new_s.info()
+# print ("----"*20)
